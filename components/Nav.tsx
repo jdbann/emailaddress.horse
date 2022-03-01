b667ff006fe1014aa3ca7dfb8d5343906f2f001c
John Bannister <johnb@hey.com>
2022-03-01 14:49:25 +0000

    Highlight active link in Nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,4 +1,5 @@
 import NextLink from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 import { styled } from "../stitches.config";
 import Cluster from "./composition/Cluster";
@@ -16,20 +17,7 @@ const Root: React.FC<React.HTMLProps<HTMLElement>> = ({
 
 const StyledRoot = styled(Root, {});
 
-type LinkProps = {
-  className?: string;
-  href: string;
-};
-
-const Link: React.FC<LinkProps> = ({ className, href, children }) => {
-  return (
-    <NextLink href={href}>
-      <a className={className}>{children}</a>
-    </NextLink>
-  );
-};
-
-const StyledLink = styled(Link, {
+const StyledLink = styled("a", {
   color: "$slate11",
   fontWeight: "$light",
   textDecoration: "none",
@@ -40,6 +28,29 @@ const StyledLink = styled(Link, {
   "&:hover": {
     color: "$slate12",
   },
+
+  variants: {
+    active: {
+      true: {
+        color: "$slate12",
+      },
+    },
+  },
 });
 
-export { StyledRoot as Root, StyledLink as Link };
+type LinkProps = {
+  className?: string;
+  href: string;
+};
+
+const Link: React.FC<LinkProps> = ({ href, children }) => {
+  const router = useRouter();
+
+  return (
+    <NextLink href={href} passHref>
+      <StyledLink active={router.asPath == href}>{children}</StyledLink>
+    </NextLink>
+  );
+};
+
+export { Root, Link };
