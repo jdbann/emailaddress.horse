b8df12ad77484020c0c3848757199e4018e03763
John Bannister <johnb@hey.com>
2022-03-01 07:23:43 +0000

    Add Fraunces font and CSS reset

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import type { AppProps } from "next/app";
+import "@fontsource/fraunces/variable-full.css";
+import { globalCss } from "../stitches.config";
+import "modern-css-reset";
+
+const globalStyles = globalCss({
+  html: {
+    fontFamily: "FrauncesVariable",
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
+  globalStyles();
   return <Component {...pageProps} />;
 }
 
