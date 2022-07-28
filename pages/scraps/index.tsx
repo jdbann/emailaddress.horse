bd0763078e3a66aec930517b0170b84b670e9f1e
John Bannister <johnb@hey.com>
2022-07-28 10:36:36 +0100

    Extract scraps store interface
    Intending to use an API to host content in future so extracting a common
    interface for all data fetching required by scraps pages.

--- pages/scraps/index.tsx
+++ pages/scraps/index.tsx
@@ -1,53 +1,40 @@
 import Link from "next/link";
 import Time from "../../components/semantic/Time";
-import { getScrapListings } from "../../lib/scrap";
+import {store} from "../../lib/scrap";
 
-import type { ScrapListing } from "../../lib/scrap";
+import type {ScrapListing} from "../../lib/scrap";
+import Layout from "../../components/Layout";
 
 type ScrapsPageProps = {
   scraps: ScrapListing[];
-  markdoc: {
-    frontmatter: {
-      title: string;
-      tagline: string;
-    };
-  };
 };
 
-const ScrapsPage = ({ scraps }: ScrapsPageProps) => {
+const ScrapsPage = ({scraps}: ScrapsPageProps) => {
   return (
     <>
-      {scraps.map(({ slug, date, title }) => {
-        return (
-          <article key={slug}>
-            <h2>
-              <Link href={`/scraps/${slug}`}>
-                <a>{title}</a>
-              </Link>
-            </h2>
-            <p>
-              <Time dateTime={new Date(date!)} />
-            </p>
-          </article>
-        );
-      })}
+      <Layout title="Scraps" tagline="Rough thoughts, hastily formed.">
+        {scraps.map(({slug, date, title}) => {
+          return (
+            <article key={slug}>
+              <h2>
+                <Link href={`/scraps/${slug}`}>
+                  <a>{title}</a>
+                </Link>
+              </h2>
+              <p>
+                <Time dateTime={new Date(date!)} />
+              </p>
+            </article>
+          );
+        })}
+      </Layout>
     </>
   );
 };
 
 export default ScrapsPage;
 
-export async function getStaticProps(): Promise<{ props: ScrapsPageProps }> {
-  const scraps = getScrapListings();
-  return {
-    props: {
-      scraps,
-      markdoc: {
-        frontmatter: {
-          title: "Scraps",
-          tagline: "Rough thoughts, hastily formed.",
-        },
-      },
-    },
-  };
+export async function getStaticProps(): Promise<{props: ScrapsPageProps}> {
+  const scraps = await store.getScrapsListings();
+  return {props: {scraps}};
 }
