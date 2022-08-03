5cba70b4f14f09facc6e4b4a2e8d1fcf2f12d72f
John Bannister <johnb@hey.com>
2022-08-03 09:18:01 +0100

    Remove stitches

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
-import { getCssText } from "../stitches.config";
 
 export default class Document extends NextDocument {
   render() {
@@ -10,10 +9,6 @@ export default class Document extends NextDocument {
           <link
             href="https://fonts.googleapis.com/css2?family=Fraunces:ital,opsz,wght,SOFT,WONK@0,9..144,100..400,1,1;1,9..144,300,1,1&display=block"
             rel="stylesheet"
-          />
-          <style
-            id="stitches"
-            dangerouslySetInnerHTML={{ __html: getCssText() }}
           />
         </Head>
         <body>
